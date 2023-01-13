5b94b29cc9a6913d64891c6db0666120a4925035
eight04 <eight04@gmail.com>
2023-01-13 12:54:23 +0800

    Change: get version number from subprocess pip instead of pkg_resource

--- pip_outdated/check_outdated.py
+++ pip_outdated/check_outdated.py
@@ -1,3 +1,4 @@
+import asyncio
 from dataclasses import dataclass
 from typing import List, Optional
 
@@ -5,7 +6,6 @@ from packaging.requirements import Requirement
 from packaging.utils import canonicalize_name
 from packaging.version import Version
 from packaging.version import parse as parse_version
-from pkg_resources import DistributionNotFound, get_distribution
 
 from .verbose import verbose
 
@@ -36,6 +36,8 @@ class OutdateResult:
         return self.version is None
         
     def install_not_wanted(self) -> bool:
+        if self.version is None:
+            return False
         return self.version not in self.requirement.specifier
         
     def pypi_not_found(self) -> bool:
@@ -44,16 +46,22 @@ class OutdateResult:
     def outdated(self) -> bool:
         return self.version != self.wanted or self.version != self.latest
 
-def get_current_version(name: str) -> Optional[Version]:
-    try:
-        return parse_version(get_distribution(name).version)
-    except DistributionNotFound:
+async def get_local_version(name: str) -> Optional[Version]:
+    p = await asyncio.create_subprocess_shell(
+        f"pip show {name}",
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+        )
+    stdout, _ = await p.communicate()
+    if p.returncode != 0:
         return None
+    for line in stdout.decode().splitlines():
+        if line.startswith("Version: "):
+            return parse_version(line[9:])
         
 async def get_pypi_versions(name: str, session) -> List[Version]:
     async with session.get(f"https://pypi.org/pypi/{name}/json") as r:
-        if r.status != 200:
-            return None
+        r.raise_for_status()
         keys = [parse_version(v) for v in (await r.json())["releases"].keys()]
         keys = [v for v in keys if not v.is_prerelease]
         keys.sort()
@@ -63,6 +71,6 @@ async def check_outdated(require, session) -> OutdateResult:
     if verbose():
         print(f"Checking: {require.name} {require.specifier}")
     name = canonicalize_name(require.name)
-    current_version = get_current_version(name)
+    current_version = await get_local_version(name)
     pypi_versions = await get_pypi_versions(name, session)
     return OutdateResult(require, current_version, pypi_versions)
