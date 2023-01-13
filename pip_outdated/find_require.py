28563b38b095c9189023a464840ae060a980c983
eight <eight04@gmail.com>
2023-01-13 17:58:56 +0800

    Change: get local version from pip CLI. Drop pkg_resources, cchardet (#22)
    * Update dependencies, drop cchardet
    
    * Change: get version number from subprocess pip instead of pkg_resource
    
    * Update python version
    
    * Drop travis, switch to github action
    
    * we don't need conditional import

--- pip_outdated/find_require.py
+++ pip_outdated/find_require.py
@@ -8,8 +8,8 @@ import re
 from itertools import chain
 
 from packaging.requirements import Requirement, InvalidRequirement
-from setuptools.config import read_configuration
-
+# https://github.com/conda/conda-build/issues/4428
+from setuptools.config.setupcfg import read_configuration
 from .verbose import verbose
 
 def iter_files(patterns):
